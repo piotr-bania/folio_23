461091a08f268332727ab3e5c73318521138611c
Piotr <piotr.bania@europe.com>
2023-03-05 21:26:47 +0000

    init

--- components/homepage/Experience.js
+++ components/homepage/Experience.js
@@ -1,13 +1,10 @@
-import React, { Suspense, useRef} from 'react'
-import Link from 'next/link'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { AnimatePresence, motion as m } from 'framer-motion'
-import { EffectComposer, Noise, Scanline, Bloom, Vignette, Glitch } from '@react-three/postprocessing'
-import gsap from 'gsap'
 import Foyer from './Foyer'
 
-
 const Experience = () => {
+
     return (
         <AnimatePresence>
             <m.div
@@ -15,28 +12,16 @@ const Experience = () => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, transition: { delay: 0.1, duration: 2 } }}
                 >
+
                 <Canvas
                     shadows
                     camera={{
-                        position: [0, 0, 20],
-                        fov: 45 }}
+                        position: [0, 0, 35],
+                        fov: 25 }}
                     >
-
                     <Suspense fallback={null}>
                         <Foyer />
                     </Suspense>
-
-                    {/* <EffectComposer>
-                        <Scanline density={2.5} opacity={0.25}/>
-                        <Noise opacity={0.15} />
-                        <Vignette eskil={false} offset={0.25} darkness={0.25} />
-                        <Glitch
-                        delay={[0.05, 5]}
-                        duration={[0.005, 0.05]}
-                        strength={[0.01, 0.05]}
-                        ratio={0.95}
-                        />
-                    </EffectComposer> */}
                 </Canvas>
             </m.div>
         </AnimatePresence>
--- components/homepage/Foyer.js
+++ components/homepage/Foyer.js
@@ -9,14 +9,25 @@ function Foyer() {
 
     const { scale } = useControls({ scale: 1 })
     const { position } = useControls({ position: [0, -2.5, 0] })
-    const { rotation } = useControls({ rotation: [0.3, -0.6, 0] })
+    const { rotation } = useControls({ rotation: [0, -0.79, 0] })
 
     const foyer = useLoader(GLTFLoader, '/models/foyer/foyer.glb')
     const foyerRef = useRef()
 
     return (
         <>
-            <OrbitControls />
+            <OrbitControls
+                rotateSpeed={0.12}
+                enableZoom={true}
+                minDistance={15}
+                maxDistance={60}
+                minZoom={0}
+                maxZoom={1}
+                minAzimuthAngle={-Math.PI / 4}
+                maxAzimuthAngle={Math.PI / 4}
+                minPolarAngle={Math.PI / 90}
+                maxPolarAngle={Math.PI - Math.PI / 2}
+            />
 
             <ambientLight intensity={0.05} color={'#ffffff'} />
             <directionalLight intensity={0.75}/>
