23e7beeba91c4d4af1783d0cc87aa2cc17e26a53
Piotr <piotr.bania@europe.com>
2023-03-06 18:36:02 +0000

    init

--- components/homepage/Foyer.js
+++ components/homepage/Foyer.js
@@ -12,7 +12,7 @@ function Foyer() {
     const { position } = useControls({ position: [0, -2.5, 0] })
     const { rotation } = useControls({ rotation: [0, -0.79, 0] })
 
-    const foyer = useLoader(GLTFLoader, '/models/foyer/foyer.glb')
+    const foyer = useLoader(GLTFLoader, '/models/foyer/foyer_entrance.glb')
     const foyerRef = useRef()
 
     const pointL = useRef()
@@ -40,11 +40,10 @@ function Foyer() {
 
             <ambientLight intensity={0.005} />
             <directionalLight intensity={0.005} />
-            <pointLight ref={pointL} intensity={2} position={[-8, 0, 0]}color={'#7161F5'} distance={5} decay={1.5} />
-            <pointLight ref={pointL} intensity={2} position={[8, 0.5, 0]}color={'#7161F5'} distance={5} decay={1.5} />
-            <pointLight ref={pointL} intensity={2} position={[ 0, 1, -7]}color={'#61F570'} distance={5} decay={1.5} />
-            <pointLight ref={pointL} intensity={2} position={[ 0, 2, 9]}color={'#61F570'} distance={5} decay={1.5} />
-            <pointLight ref={pointL} intensity={2} position={[ 0, -2, 0]}color={'#FFFFFF'} distance={8} decay={1.5} />
+            <pointLight ref={pointL} intensity={1} position={[-8, 0, 0]}color={'#FB6C3F'} distance={6} decay={2.5}  />
+            <pointLight ref={pointL} intensity={1} position={[8, 0.5, 0]}color={'#FB6C3F'} distance={6} decay={2.5}  />
+            <pointLight ref={pointL} intensity={1} position={[ 0, 1, -7]}color={'#5DA1A2'} distance={6} decay={2.5}  />
+            <pointLight ref={pointL} intensity={1} position={[ 0, 2, 9]}color={'#F8FA57'} distance={6} decay={2.5} />
 
             <mesh ref={foyerRef}
                 scale={scale} position={position} rotation={rotation}
