d38258d78e594f850e1071ef9e0a4933f97da0a4
Piotr <piotr.bania@europe.com>
2023-03-06 21:26:20 +0000

    init

--- components/homepage/Foyer.js
+++ components/homepage/Foyer.js
@@ -3,7 +3,7 @@ import { useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useControls } from 'leva'
 import * as THREE from 'three'
-import { useHelper, OrbitControls, MeshReflectorMaterial } from '@react-three/drei'
+import { useHelper, OrbitControls, Environment, Lightformer } from '@react-three/drei'
 import { PointLightHelper, SpotLightHelper } from 'three'
 
 function Foyer() {
@@ -38,13 +38,15 @@ function Foyer() {
             {/* <spotLight /> */}
             {/* <rectAreaLight /> */}
 
-            <ambientLight intensity={0.005} />
-            <directionalLight intensity={0.005} />
+            {/* <ambientLight intensity={0.05} />
+            <directionalLight intensity={0.05} /> */}
             <pointLight ref={pointL} intensity={1} position={[-8, 0, 0]}color={'#FB6C3F'} distance={6} decay={2.5}  />
             <pointLight ref={pointL} intensity={1} position={[8, 0.5, 0]}color={'#FB6C3F'} distance={6} decay={2.5}  />
             <pointLight ref={pointL} intensity={1} position={[ 0, 1, -7]}color={'#5DA1A2'} distance={6} decay={2.5}  />
             <pointLight ref={pointL} intensity={1} position={[ 0, 2, 9]}color={'#F8FA57'} distance={6} decay={2.5} />
             <pointLight ref={pointL} intensity={3} position={[ 4.5, 1.5, -3.5]}color={'#FFFFFF'} distance={5} decay={3.5} />
+
+            <Environment files='./environments/dikhololo_night_1k.hdr' />
 
             <mesh ref={foyerRef}
                 scale={scale} position={position} rotation={rotation}
